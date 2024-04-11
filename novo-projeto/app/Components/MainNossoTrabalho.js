19bf1c84049c2dff826cbb71c62dab370521454b
Vinicius Ambosano Melo <83926963+Vinimelo10@users.noreply.github.com>
2024-04-11 14:41:49 +0000

    changes

---
+++ novo-projeto/app/Components/MainNossoTrabalho.js
@@ -0,0 +1,16 @@
+export default function MainNossoTrabalho() {
+    return (
+        <main>
+            <div className="NossosTrabalhos">
+            <h1>Nossos Serviços</h1>
+            <div>
+                <h2>Conscientização e compromisso político</h2>
+                <br /><br />
+                <p>O Brasil já reconhece a importância de abordar a poluição do ar e da água, assim como o uso de produtos químicos perigosos. O governo brasileiro tem demonstrado seu compromisso com a saúde pública ao implementar políticas e regulamentos relacionados à proteção ambiental e à saúde.
+                Infraestrutura e recursos: O Brasil possui uma infraestrutura sólida e recursos humanos qualificados para monitorar e avaliar a poluição do ar e da água, além de gerenciar o uso de produtos químicos perigosos. Isso facilitará a implementação de medidas eficazes para atingir o alvo 3.9.
+                Parcerias e colaboração: O Brasil pode se beneficiar de parcerias e colaborações internacionais para compartilhar conhecimentos, tecnologias e boas práticas em relação às questões abordadas neste alvo. A colaboração entre setores público, privado e da sociedade civil pode também ser fundamental para garantir o sucesso neste objetivo.</p>
+            </div>
+            </div>
+        </main>
+    );
+}
